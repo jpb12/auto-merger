12011e9c51c034301614ceeae9e0dbf486306fb0
james <jpb12@hotmail.co.uk>
2015-11-17 22:19:51 +0000

    Core project and config types

---
+++ BranchManager/BranchManager.Core/Types/Merge.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Serialization;
+
+namespace BranchManager.Core.Types
+{
+	public class Merge
+	{
+		[XmlAttribute("Parent")]
+		public string Parent { get; set; }
+
+		[XmlAttribute("Child")]
+		public string Child { get; set; }
+
+		[XmlAttribute("Enabled")]
+		public bool Enabled { get; set; }
+	}
+}
