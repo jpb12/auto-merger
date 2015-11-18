f71a31a28fe734b91fc70233c845ad4e694d4088
james <jpb12@hotmail.co.uk>
2015-11-18 23:14:39 +0000

    Web API controller with dummy data

---
+++ BranchManager/BranchManager/Api/TreeController.cs
@@ -0,0 +1,46 @@
+﻿using BranchManager.Core.Types;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace BranchManager.Api
+{
+	[RoutePrefix("api/tree")]
+	public class TreeController : ApiController
+	{
+		[Route("")]
+		public MergeConfig Get()
+		{
+			return new MergeConfig
+			{
+				Projects = new List<Project>
+				{
+					new Project
+					{
+						ProjectUrl = "http://temp/one",
+						Merges = new List<Merge>
+						{
+							new Merge
+							{
+								Child = "trunk",
+								Parent = "1.0",
+								Enabled = true
+							},
+							new Merge
+							{
+								Child = "child-one",
+								Parent = "trunk",
+								Enabled = true
+							},
+							new Merge
+							{
+								Child = "child-two",
+								Parent = "trunk",
+								Enabled = true
+							}
+						}
+					}
+				}
+			};
+		}
+	}
+}
--- BranchManager/BranchManager/App_Start/WebApiConfig.cs
+++ BranchManager/BranchManager/App_Start/WebApiConfig.cs
@@ -7,8 +7,13 @@ namespace BranchManager
 	{
 		public static void Register(HttpConfiguration config)
 		{
-			config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+			config.Formatters.Remove(config.Formatters.XmlFormatter);
 
+#if DEBUG
+			config.Formatters.JsonFormatter.Indent = true;
+#endif
+
+			config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
 			config.MapHttpAttributeRoutes();
 		}
 	}
