2efce1bc423cea5cb2cc9b356d0453c92317668c
james <jpb12@hotmail.co.uk>
2016-01-03 00:34:11 +0000

    Indicating disabled merge with a dashed line

--- BranchManager/BranchManager/JS/Components/Link.js
+++ BranchManager/BranchManager/JS/Components/Link.js
@@ -1,10 +1,14 @@
 ﻿var Link = React.createClass({
+	branch: function() {
+		return this.props.link.source.branches.filter(branch => branch.child == this.props.link.target)[0];
+	},
 	displayName: 'Link',
 	render: function () {
 		return (
 			React.createElement(
 				'path',
 				{
+					className: this.branch().enabled ? 'enabled' : 'disabled',
 					d: this.props.diagonal({ source: this.props.link.source, target: this.props.link.target }),
 					transform: 'translate(' + this.props.margins.top + ', ' + this.props.margins.left + ')'
 				}))
