37124341640f62fd8a17874fa95ae1cbbb961d3b
james <jpb12@hotmail.co.uk>
2016-01-03 23:52:12 +0000

    Allowing multiple root nodes

--- BranchManager/BranchManager/JS/Stores/TreeData.js
+++ BranchManager/BranchManager/JS/Stores/TreeData.js
@@ -24,7 +24,20 @@ var treeDataStore = Reflux.createStore({
 			.size([this.height, this.width])
 			.children(node => node.branches.map(item => item.child));
 
-		var nodes = tree.nodes(this.currentTree);
+		// d3 trees can only have one root node, so to show the merge tree when there are multiple nodes
+		// we add a new parent of all the roots, which we do not show
+		var parentNode = {
+			branches: this.currentTree.roots.map(node => ({ child: node }))
+		};
+
+		var allNodes = tree.nodes(parentNode);
+		var nodes = allNodes.slice(1);
+
+		// we need to remove the offset caused by the temporary parent, and then scale the nodes to fill
+		// the full svg
+		var depth = Math.max.apply(null, nodes.map(node => node.depth));
+		nodes.forEach(node => node.y = (node.y - this.width / depth) * (depth / (depth - 1)));
+
 		var links = tree.links(nodes);
 
 		this.result = {
@@ -45,7 +58,7 @@ var treeDataStore = Reflux.createStore({
 		this.margins = {};
 
 		$.ajax({ url: "api/tree" }).done(data => {
-			this.currentTree = data[0].roots[0];
+			this.currentTree = data[0];
 			this.redraw();
 		});
 	},
