2f76fb50978e92f3c7a43b961b83be3fc7fb5b73
james <jpb12@hotmail.co.uk>
2016-01-06 00:52:16 +0000

    Colouring tree using gradient

--- BranchManager/BranchManager/JS/Components/Tree/Link.js
+++ BranchManager/BranchManager/JS/Components/Tree/Link.js
@@ -5,14 +5,45 @@ Components.Link = React.createClass({
 		return this.props.link.source.branches.filter(branch => branch.child == this.props.link.target)[0];
 	},
 	displayName: 'Link',
-	diagonal: d3.svg.diagonal().projection(function (d) { return [d.y, d.x]; }),
-	render: function () {
+	diagonal: d3.svg.diagonal().projection(function(d) { return [d.y, d.x]; }),
+	getColour: function () {
+		var colours = [
+			{ r: 42, g: 243, b: 39, percent: 0 },
+			{ r: 2, g: 187, b: 170, percent: 0.25 },
+			{ r: 239, g: 0, b: 255, percent: 0.35 },
+			{ r: 210, g: 0, b: 145, percent: 0.55 },
+			{ r: 173, g: 0, b: 0, percent: 1 }
+		];
+
+		var percent = Math.min((this.branch().unmergedRevisions / this.props.commits), 1);
+
+		for (var i = 0; i < colours.length; i++) {
+			var firstColour = colours[i];
+			var secondColour = colours[i + 1];
+
+			if (secondColour.percent < percent) {
+				continue;
+			}
+
+			var percentAlong = (percent - firstColour.percent) / (secondColour.percent - firstColour.percent);
+
+			var red = Math.round(firstColour.r + (secondColour.r - firstColour.r) * percentAlong);
+			var blue = Math.round(firstColour.b + (secondColour.b - firstColour.b) * percentAlong);
+			var green = Math.round(firstColour.g + (secondColour.g - firstColour.g) * percentAlong);
+
+			return 'rgb(' + red + ', ' + blue + ', ' + green + ')';
+		}
+	},
+	render: function() {
 		return (
 			React.createElement(
 				'path',
 				{
 					className: this.branch().enabled ? 'enabled' : 'disabled',
+					stroke: this.getColour(),
 					d: this.diagonal({ source: this.props.link.source, target: this.props.link.target })
 				}))
 	}
 });
+
+Components.Link = ReactRedux.connect(state => ({ commits: state.settings.commits }))(Components.Link);
